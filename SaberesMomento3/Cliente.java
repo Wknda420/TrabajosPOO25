3f2b3eed08be821110527bc51ec2fda19ea78e91
Wanda <wanda.montanobet@campusucc.edu.co>
2025-04-28 22:14:10 +0000

    constructor cliente

--- SaberesMomento3/Cliente.java
+++ SaberesMomento3/Cliente.java
@@ -1,6 +1,14 @@
 package SaberesMomento3;
 
 class Cliente {
-    
+    private String cedula;
+    private String nombre;
 
+    //Esta clase representa un cliente con cedula y nombre
+
+    //Constructor clase cliente
+    public Cliente (String cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+    }
 }
