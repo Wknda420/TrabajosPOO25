1e96d6d501c2d770e967ce22b7181d7897eecb71
Wanda <wanda.montanobet@campusucc.edu.co>
2025-04-28 22:16:15 +0000

    getters y setters de la clase cliente

--- SaberesMomento3/Cliente.java
+++ SaberesMomento3/Cliente.java
@@ -11,4 +11,21 @@ class Cliente {
         this.cedula = cedula;
         this.nombre = nombre;
     }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
 }
