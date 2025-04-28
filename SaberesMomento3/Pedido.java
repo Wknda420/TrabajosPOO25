d357aed9f49d889b49186f3264aeca6beaebf276
Wanda <wanda.montanobet@campusucc.edu.co>
2025-04-28 22:54:26 +0000

    fecha en la clase pedido

--- SaberesMomento3/Pedido.java
+++ SaberesMomento3/Pedido.java
@@ -3,4 +3,13 @@ package SaberesMomento3;
 public class Pedido {
     private Cliente cliente;
     private Producto producto;
+    /*esta clase representa un pedido, 
+    se relaciona mediante agregacion con clase cliente y producto*/
+
+    private java.util.Date fecha;
+    /* No estoy importando ninguna libreria entonces
+    debo usar todo el nombre (java.util) si no lo uso
+    me aparecia un error por compilacion */
+
+    
 }
