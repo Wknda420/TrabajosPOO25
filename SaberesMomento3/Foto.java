943e41e61d7536e2324f9960d8ce8f5698c799dd
Wanda <wanda.montanobet@campusucc.edu.co>
2025-04-28 23:45:48 +0000

    detalles

--- SaberesMomento3/Foto.java
+++ SaberesMomento3/Foto.java
@@ -2,4 +2,7 @@ package SaberesMomento3;
 
 public class Foto {
     private String fichero;
+
+    /*esta clase representa la foto se puede realcionar
+    mediante herencia con la clase impresion*/
 }
