ac637a28ab8e1c40cbc6138bf8a7e49c8df23339
Wanda <wanda.montanobet@campusucc.edu.co>
2025-04-29 01:14:52 +0000

    agrgacion - ejecutar

--- SaberesMomento3/Ejecutar.java
+++ SaberesMomento3/Ejecutar.java
@@ -1,5 +1,10 @@
 package SaberesMomento3;
 
 public class Ejecutar{//Clase para pruebas principal
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente("1043653622", "Nicole");
+        Producto producto = new Producto(111);
+        Pedido pedido = new Pedido(cliente, producto, new java.util.Date(), 153684482);
 
+        //ejemplo de agregacion
 }
