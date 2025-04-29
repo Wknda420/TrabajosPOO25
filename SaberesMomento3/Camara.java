b61bb8a1e8944328eb743dedf08fab19228bbf27
Wanda <wanda.montanobet@campusucc.edu.co>
2025-04-29 01:09:42 +0000

    get y set de la clase camara

--- SaberesMomento3/Camara.java
+++ SaberesMomento3/Camara.java
@@ -13,5 +13,22 @@ public class Camara {
         this.modelo = modelo;
     }
 
+    //get y set
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
     
 }
