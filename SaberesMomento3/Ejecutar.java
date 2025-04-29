bad673453b00a290ad9e8e507b6bd4f2bbb6fd80
Wanda <wanda.montanobet@campusucc.edu.co>
2025-04-29 01:17:22 +0000

    herencia y polimorfismo ejecutar

--- SaberesMomento3/Ejecutar.java
+++ SaberesMomento3/Ejecutar.java
@@ -5,6 +5,10 @@ public class Ejecutar{//Clase para pruebas principal
         Cliente cliente = new Cliente("1043653622", "Nicole");
         Producto producto = new Producto(111);
         Pedido pedido = new Pedido(cliente, producto, new java.util.Date(), 153684482);
+    //ejemplo de agregacion
 
-        //ejemplo de agregacion
+
+    //herencia y polimorfismo
+    Foto foto1 = new Foto ("Eladio.jpg");
+    foto1.print(); //metodo foto
 }
