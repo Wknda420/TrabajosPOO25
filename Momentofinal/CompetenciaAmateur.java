fa079d05f7ecae26edda9b420c73bfc90e2c3b0c
Wanda <wanda.montanobet@campusucc.edu.co>
2025-05-19 14:33:27 +0000

    correcion de separacion  clase main

--- Momentofinal/CompetenciaAmateur.java
+++ Momentofinal/CompetenciaAmateur.java
@@ -1,203 +1,15 @@
 package Momentofinal;
 
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 
-/*La clase que va a representar un competidor amater*/
-
-abstract class Competidor {
-    protected String nombre;
-    protected double peso; //peso en kg
-    protected String estilo;
-    protected int resistencia;
-    protected int golpesEfectivos;
-    protected int puntuacionJueces;
-
-    protected static final int RESISTENCIA_INICIAL = 100;
-    protected static final int MAX_GOLPE = 25;
-    protected static final int MIN_GOLPE = 5;
-
-    public Competidor(String nombre, double peso, String estilo) {
-        this.nombre = nombre;
-        this.peso = peso;
-        this.estilo = estilo;
-        this.resistencia = RESISTENCIA_INICIAL;
-        this.golpesEfectivos = 0;
-        this.puntuacionJueces = 0;
-    }
-
-    
-    public abstract void atacar(Competidor oponente);
-    public abstract int defender();
-
-    public void recibirGolpe(int daño) {
-        int dañoRecibido = (int)(daño*(1.0 - this.defender() / 100.0));
-        this.resistencia -= dañoRecibido;
-        if (this.resistencia <0) this.resistencia = 0;
-
-    }
-
-    //getters
-
-    public boolean estaEnPie() {
-        return this.resistencia > 20;
-    }
-
-    public void añadirPuntuacion(int puntos) {
-        this.puntuacionJueces += puntos;
-    }
-
-
-    public String getNombre() {
-        return nombre;
-    }
-
-
-    public double getPeso() {
-        return peso;
-    }
-
-
-    public String getEstilo() {
-        return estilo;
-    }
-
-
-    public int getResistencia() {
-        return resistencia;
-    }
-
-
-    public int getGolpesEfectivos() {
-        return golpesEfectivos;
-    }
-
-
-    public int getPuntuacionJueces() {
-        return puntuacionJueces;
-    }
-}
-
-/*Esta clase representa a un competidor de kickboxing*/
-
-class Kickboxing extends Competidor {
-
-    public Kickboxing(String nombre, double peso) {
-        super (nombre, peso, "Kickboxing");
-    }
-
-    @Override
-    public void atacar(Competidor oponente) {
-        Random rand = new Random();
-        double modificadorPeso= this.peso / 70.0;
-        double modificadorResistencia = this.resistencia / 100.0;
-
-        int dañoBase = rand.nextInt((MAX_GOLPE - MIN_GOLPE) + 1) + MIN_GOLPE;
-        //Kickboxing tiene ataques balanceados
-
-        if (rand.nextDouble()> 0.5) {
-            dañoBase += 3; //ataque balanceado
-
-            System.out.println(this.nombre + " Realizo una combinacion de patada y puño!");
-        }
-
-        int daño = (int)(dañoBase * modificadorPeso * modificadorResistencia);
-
-        oponente.recibirGolpe(daño);
-        this.golpesEfectivos++;
-
-        this.resistencia -= rand.nextInt (10) + 5; //el desgaste medio
-
-        if (this.resistencia < 0) this.resistencia = 0;
-
-        System.out.println(this.nombre + " Ataca con tecnica de Kickboxing a " + oponente.getNombre() + " (" + daño + " Puntos de daño). Resistencia: " + this.resistencia);
-
-    }
-
-    @Override
-    public int defender() {
-        return new Random().nextInt(30); //defensa media(30%)
-    }
-}
-
-/*esta clase representa un competidor de muaythai*/
-
-class MuayThai extends Competidor {
-    public MuayThai (String nombre, double peso) {
-        super(nombre, peso, "Muay Thai");
-    }
-
-    @Override
-    public void atacar(Competidor oponente) { 
-        Random rand = new Random();
-        double modificadorPeso = this.peso / 70.0;
-        double modificadorResistencia = this.resistencia / 100.0;
-
-        int dañoBase = rand.nextInt((MAX_GOLPE - MIN_GOLPE) + 1) + MIN_GOLPE + 5; //+5 por estilo
-
-        int daño = (int) (dañoBase * modificadorPeso * modificadorResistencia);
-
-        oponente.recibirGolpe(daño);
-        this.golpesEfectivos++;
-
-        this.resistencia -= rand.nextInt(12) + 5; //Desgaste mayor
-        if (this.resistencia < 0) this.resistencia = 0;
-
-        System.out.println(this.nombre + " Lanza una patada con potencia de muay thai a " + oponente.getNombre() + " (" + daño + " puntos de daño). Resistencia: " + this.resistencia);
-    }
-
-    @Override
-    public int defender() {
-        return new Random().nextInt(20); //defensa menor (20%)
-    }
-
-}
-
-/*la clase representa un competidor de boxeo*/
-
-class Boxeo extends Competidor {
-    public Boxeo(String nombre, double peso) {
-        super(nombre, peso, "Boxeo");
-    }
-
-    @Override
-    public void atacar (Competidor oponente) {
-        Random rand = new Random();
-        double modificadorPeso = this.peso / 70.0;
-        double modificadorResistencia = this.resistencia / 100.0;
-
-        int dañoBase = rand.nextInt((MAX_GOLPE - MIN_GOLPE) + 1) + MIN_GOLPE;
-        if(rand.nextDouble() > 0.6) {
-            dañoBase += 5; //Combinacion
-            System.out.println(this.nombre + " conecto una combinacion rapida de puños!");
-        }
-
-        int daño = (int)(dañoBase * modificadorPeso * modificadorResistencia);
-
-        oponente.recibirGolpe(daño);
-        this.golpesEfectivos++;
-
-        this.resistencia -= rand.nextInt(10) + 4; //desgaste medio
-        if (this.resistencia < 0) this.resistencia = 0;
-
-        System.out.println(this.nombre + " golpea con tecnica de Boxeo a " + oponente.getNombre() + " (" + daño + " puntos de daño). Resistencia: " + this.resistencia);
-    }
-
-    @Override
-    public int defender() { 
-        return new Random().nextInt(35); //Buena dfensa(35%)
-    }
-}
-
-/*esta clase maneja la competencia*/
-
-class CompetenciaAmater{
+public class CompetenciaAmateur {
     private Competidor esquinaRoja;
     private Competidor esquinaAzul;
     private int asaltoActual;
     private static final int TOTAL_ASALTOS = 3; 
 
-    public CompetenciaAmater(Competidor esquinaRoja, Competidor esquinaAzul) {
+    public CompetenciaAmateur(Competidor esquinaRoja, Competidor esquinaAzul) {
         this.esquinaRoja = esquinaRoja;
         this.esquinaAzul = esquinaAzul;
         this.asaltoActual = 1;
@@ -210,16 +22,12 @@ class CompetenciaAmater{
         System.out.println(esquinaAzul.getNombre() + "(" + esquinaAzul.getEstilo() +  ", " + esquinaAzul.getPeso() + "kg)");
         System.out.println("-----------------------------------------------------------------------------------------------");
 
-        /*esta parte controla el bucle del combate o se alos asalto y descansos entre rounds */
         while (asaltoActual <= TOTAL_ASALTOS && esquinaRoja.estaEnPie() && esquinaAzul.estaEnPie()) {
             ejecutarAsalto();
             puntuarAsalto();
-            asaltoActual++; //aumenta los asaltos ejecutados que son de 1 a 3
+            asaltoActual++;
 
-
-            /*descanso de los asaltos */
             if (asaltoActual <= TOTAL_ASALTOS && esquinaRoja.estaEnPie() && esquinaAzul.estaEnPie()) {
-
                 System.out.println("----- DESCANSO ENTRE ASALTO -----");
                 esquinaRoja.resistencia += 15;
                 esquinaAzul.resistencia += 15;
@@ -232,7 +40,6 @@ class CompetenciaAmater{
     }
 
     private void ejecutarAsalto() {
-
         System.out.println(" ASALTO " + asaltoActual + " - ¡GO!");
 
         int accionesPorAsalto = 5;
@@ -253,44 +60,40 @@ class CompetenciaAmater{
         }
     }
 
-        private void puntuarAsalto() {
+    private void puntuarAsalto() {
+        Random rand = new Random();
+        int puntos1 = esquinaRoja.getGolpesEfectivos() * 2 + esquinaRoja.getResistencia() / 10 + rand.nextInt(5);
+        int puntos2 = esquinaAzul.getGolpesEfectivos() * 2 + esquinaAzul.getResistencia() / 10 + rand.nextInt(5);
 
-            Random rand = new Random();
-            int puntos1 = esquinaRoja.getGolpesEfectivos() * 2 + esquinaRoja.getResistencia() / 10 + rand.nextInt(5);
-            int puntos2 = esquinaAzul.getGolpesEfectivos() * 2 + esquinaAzul.getResistencia() / 10 + rand.nextInt(5);
+        esquinaRoja.añadirPuntuacion(puntos1);
+        esquinaAzul.añadirPuntuacion(puntos2);
 
-            esquinaRoja.añadirPuntuacion(puntos1);
-            esquinaAzul.añadirPuntuacion(puntos2);
+        System.out.println(" Puntuacion del asalto " + asaltoActual + ": ");
+        System.out.println(esquinaRoja.getNombre() + ": " + puntos1 + " puntos");
+        System.out.println(esquinaAzul.getNombre() + ": " + puntos2 + " puntos");
+    }
 
-            System.out.println(" Puntuacion del asalto " + asaltoActual + ": ");
-            System.out.println(esquinaRoja.getNombre() + ": " + puntos1 + " puntos");
-            System.out.println(esquinaAzul.getNombre() + ": " + puntos2 + " puntos");
-        }
+    private void mostrarResultado() {
+        System.out.println("---------RESULTADO FINAL---------");
 
-        private void mostrarResultado() {
-            System.out.println("---------RESULTADO FINAL---------");
+        if (!esquinaRoja.estaEnPie() && !esquinaAzul.estaEnPie()) {
+            System.out.println("¡Doble KO! El combate se empata.");
+        }else if (!esquinaAzul.estaEnPie()) {
+            System.out.println("¡KO! " + esquinaAzul.getNombre() + " gana el combate!");
+        } else { 
+            System.out.println("Desicion de los jueces: ");
+            System.out.println(esquinaRoja.getNombre() + ": " + esquinaRoja.getPuntuacionJueces() + " puntos");
+            System.out.println(esquinaAzul.getNombre() + ": " + esquinaAzul.getPuntuacionJueces() + " puntos");
 
-            if (!esquinaRoja.estaEnPie() && !esquinaAzul.estaEnPie()) {
-                System.out.println("¡Doble KO! El combate se empata.");
-            }else if (!esquinaAzul.estaEnPie()) {
-                System.out.println("¡KO! " + esquinaAzul.getNombre() + " gana el combate!");
-            } else { 
-                System.out.println("Desicion de los jueces: ");
-                System.out.println(esquinaRoja.getNombre() + ": " + esquinaRoja.getPuntuacionJueces() + " puntos");
-                System.out.println(esquinaAzul.getNombre() + ": " + esquinaAzul.getPuntuacionJueces() + " puntos");
-
-                if(esquinaRoja.getPuntuacionJueces() > esquinaAzul.getPuntuacionJueces()) {
-                    System.out.println("¡Ganador por decision unanime: " + esquinaRoja.getNombre() + " !");
-                }else if (esquinaAzul.getPuntuacionJueces() > esquinaRoja.getPuntuacionJueces()) {
-                    System.out.println("¡Ganador por desicion dividida: " + esquinaAzul.getNombre() + "!");
-                }else {
-                    System.out.println("¡El combate termina en EMPATE!");
-                }
+            if(esquinaRoja.getPuntuacionJueces() > esquinaAzul.getPuntuacionJueces()) {
+                System.out.println("¡Ganador por decision unanime: " + esquinaRoja.getNombre() + " !");
+            }else if (esquinaAzul.getPuntuacionJueces() > esquinaRoja.getPuntuacionJueces()) {
+                System.out.println("¡Ganador por desicion dividida: " + esquinaAzul.getNombre() + "!");
+            }else {
+                System.out.println("¡El combate termina en EMPATE!");
             }
         }
-    
-
-    /*ejecutar */
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -321,7 +124,7 @@ class CompetenciaAmater{
         Competidor e1 = crearCompetidor(estilo1, nombre1, peso1);
         Competidor e2 = crearCompetidor(estilo2, nombre2, peso2);
 
-        CompetenciaAmater competencia = new CompetenciaAmater(e1, e2);
+        CompetenciaAmateur competencia = new CompetenciaAmateur(e1, e2);
 
         competencia.iniciarCombate();
 
@@ -336,4 +139,4 @@ class CompetenciaAmater{
             default: return new Kickboxing(nombre, peso);
         }
     }
- }
+}
