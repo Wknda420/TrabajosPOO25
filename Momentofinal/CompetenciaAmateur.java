b155770850144c244e5d64f16facf2d15aaa8ee8
Wanda <wanda.montanobet@campusucc.edu.co>
2025-05-19 01:43:26 +0000

    calculo de daño, puntuacion de jueces, daño recibido

--- Momentofinal/CompetenciaAmateur.java
+++ Momentofinal/CompetenciaAmateur.java
@@ -16,13 +16,32 @@ abstract class Competidor {
     protected static final int MAX_GOLPE = 25;
     protected static final int MIN_GOLPE = 5;
 
-    public Competidor(String nombre, douuble peso, String estilo) {
+    public Competidor(String nombre, double peso, String estilo) {
         this.nombre = nombre;
         this.peso = peso;
         this.estilo = estilo;
         this.resistencia = RESISTENCIA_INICIAL;
         this.golpesEfectivos = 0;
         this.puntuacionJueces = 0;
+    }
+
+    
+    public abstract void atacar(Competidor oponente);
+    public abstract int defender();
+
+    public void recibirGolpe(int daño) {
+        int dañoRecibido = (int)(daño*(1.0 - this.defender() / 100.0));
+        this.resistencia -= dañoRecibido;
+        if (this.resistencia <0) this.resistencia = 0;
+
+    }
+
+    public boolean estaEnPie() {
+        return this.resistencia > 20;
+    }
+
+    public void añadirPuntuacion(int puntos) {
+        this.puntuacionJueces += puntos;
     }
 
 }
